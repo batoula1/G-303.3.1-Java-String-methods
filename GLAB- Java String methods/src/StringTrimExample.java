580b29ec49f9ec79da515ceeabccbd252c7a444c
batoula1 <102437354+batoula1@users.noreply.github.com>
2024-07-16 22:51:07 -0400

    Trim method

--- GLAB- Java String methods/src/StringTrimExample.java
+++ GLAB- Java String methods/src/StringTrimExample.java
@@ -1,2 +1,11 @@
 public class StringTrimExample{
     public static void main(String args[]){
+        public class StringTrimExample{
+            public static void main(String args[]){
+                String s1="  hello   ";
+                System.out.println(s1+"how are you");        // without trim()
+                System.out.println(s1.trim()+"how are you"); // with trim()
+            }
+        }
+
+    }
